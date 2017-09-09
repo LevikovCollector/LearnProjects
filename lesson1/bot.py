5fe0f24297d5ec65d55ff6439a7fdf92d043143c
Vladimir <v.v.levikov@gmail.com>
2017-09-09 18:08:48 +0300

    Обновлен бот

--- lesson1/bot.py
+++ lesson1/bot.py
@@ -16,8 +16,10 @@ def main():
     dp = updater.dispatcher
     dp.add_handler(CommandHandler('start',greet_user)) # функция вызывается на команду start
     #dp.add_handler(MessageHandler(Filters.text, echo_message))
-    dp.add_handler(CommandHandler('planet', planet_info))
-    dp.add_handler(MessageHandler(Filters.text, check_constellation))
+    #dp.add_handler(CommandHandler('planet', planet_info))
+    #dp.add_handler(MessageHandler(Filters.text, check_constellation))
+    dp.add_handler(CommandHandler('wordcount', count_words))
+    dp.add_handler(CommandHandler('calc' , calculete, pass_args = True))
 
     updater.start_polling()
     updater.idle()
@@ -61,5 +63,77 @@ def check_constellation(bot, update):
         update.message.reply_text(ephem.constellation(planet)[1])
     else:
         update.message.reply_text('Введена неизвестная планета')
+
+
+def count_words(bot, update):
+    raw_text_from_user = str(update.message.text).lower()
+    first_position = raw_text_from_user.find('"')
+    last_position = raw_text_from_user.rfind('"')
+    if first_position != last_position:
+        text_from_user = raw_text_from_user [first_position:last_position+1]
+        if(text_from_user != '' or text_from_user is not None):
+            if text_from_user[0] == '"' and text_from_user[-1] == '"':
+                words = text_from_user.split(' ')
+                update.message.reply_text('Количеств слов: {0}'.format(str(len(words))))
+            else:
+              update.message.reply_text('Введите команду правильно /wordcount "<Ваш текст>"')
+        else:
+              update.message.reply_text('Введите команду правильно /wordcount "<Ваш текст>"')
+    else:
+        update.message.reply_text('Введите команду правильно /wordcount "<Ваш текст>"')
+
+def calculete(bot,update, args):
+    raw_user_message = ''.join(args).replace(' ','')
+    if raw_user_message[-1] == '=':
+        arithmetic_expression = raw_user_message.split('=')[0]
+        arithmetic_result = None
+        try:
+            if(arithmetic_expression.find('+')!= -1):
+                arithmetic_values = arithmetic_expression.split('+')
+                arithmetic_result = execute_operation(arithmetic_values, '+')
+
+            elif(arithmetic_expression.find('-')!= -1):
+                arithmetic_values = arithmetic_expression.split('-')
+                arithmetic_result = execute_operation(arithmetic_values , '-')
+            elif(arithmetic_expression.find('/')!= -1):
+                arithmetic_values = arithmetic_expression.split('/')
+                arithmetic_result = execute_operation(arithmetic_values , '/')
+            elif (arithmetic_expression.find('*') != -1):
+                arithmetic_values = arithmetic_expression.split('*')
+                arithmetic_result = execute_operation(arithmetic_values , '*')
+
+            else:
+                update.message.reply_text('Не известная команда ')
+
+            if(arithmetic_result is not None):
+                update.message.reply_text('Результат: {0}'. format(arithmetic_result))
+        except ZeroDivisionError:
+            update.message.reply_text('Деление на 0 запрещено! ')
+    else:
+        update.message.reply_text('Команда должна заканчиваться знаком равно ')
+
+def check_value_type(value):
+    if(value.find('.') == -1 and value.find(',') == -1):
+        return int(value)
+    else:
+        return float(value.replace(',', '.'))
+
+def execute_operation(values, operation):
+    have_empty_values = False
+    for value in values:
+        if(value == ''):
+            have_empty_values = True
+            break
+    if(not(have_empty_values)):
+        if(operation == '+'):
+            return check_value_type(values[0]) + check_value_type(values[1])
+        elif (operation == '-'):
+            return check_value_type(values[0]) - check_value_type(values[1])
+        elif (operation == '/'):
+            return check_value_type(values[0]) / check_value_type(values[1])
+        elif (operation == '*'):
+            return check_value_type(values[0]) * check_value_type(values[1])
+    else:
+        return "Есть пустые значения: {0}".format(values)
 
 main()
